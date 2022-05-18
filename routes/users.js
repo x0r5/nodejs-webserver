e2eeb3874ad7f94e10a28a713f0da24c2072e5db
x0r5 <obivan05@gmail.com>
2022-05-18 08:08:45 +0200

    CRUD requests with express

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,20 @@ router.get('/new', (req, res)=>{
     res.send('User new form');
 })
 
+router.post('/', (req, res)=>{
+    res.send('Create user post');
+})
+
+router.get('/:id', (req, res)=>{
+    res.send(`User get: ${req.params.id}`);
+})
+
+router.put('/:id', (req, res)=>{
+    res.send(`Update user with id: ${id}`);
+})
+
+router.delete('/:id', (req, res)=>{
+    res.send(`Delete user with id: ${id}`);
+})
+
 module.exports = router;
