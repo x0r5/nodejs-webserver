b478562ab830e256d0d587cf85e732ff978ed710
x0r5 <obivan05@gmail.com>
2022-05-29 14:52:43 +0200

    dotenv for dev

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, {});
+
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', ()=>console.log('Connected to MongoDB'));
 app.set('view engine', 'ejs'); //or we can use pug
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
